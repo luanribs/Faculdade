92cf85a6fd8579d877f36569b9ff096bc4462fd7
luanribs <140925294+luanribs@users.noreply.github.com>
2024-06-16 16:04:00 -0300

    Update Lista06_07_ExeVetor07.java

--- Listas/Lista 6/Lista06_07_ExeVetor07.java
+++ Listas/Lista 6/Lista06_07_ExeVetor07.java
@@ -1,9 +1,3 @@
-/*Ler um vetor X de 10 elementos (ler pelo teclado os 10 elementos do tipo
-inteiro). Crie um vetor Y da seguinte forma: (a) os elementos de ordem par de Y (elementos
-com índice 0, 2, 4, 6, 8) receberão os respectivos elementos de X multiplicados por 2; (b)
-Os elementos de ordem ímpar de Y (elementos com índices 1, 3, 5, 7, 9) receberão os
-respectivos elementos de X multiplicados por 3. Escrever o vetor Y.*/
-
 package Lista6;
 
 import java.util.Scanner;
