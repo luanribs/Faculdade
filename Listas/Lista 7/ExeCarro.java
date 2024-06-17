9a40f206ae2f3223df03f97d5e6eb947e4ec66f4
luanribs <140925294+luanribs@users.noreply.github.com>
2024-06-17 09:09:14 -0300

    Update ExeCarro.java

--- Listas/Lista 7/ExeCarro.java
+++ Listas/Lista 7/ExeCarro.java
@@ -1,13 +1,13 @@
 package Lista7;
 
 public class ExeCarro {    public static void main(String[] args) throws Exception {
-    Propietario primeiroPropietario = new Propietario("Luan", "Rua minha vida é um inferno", 'm', "234.256.234-82", "122");
+    Propietario primeiroPropietario = new Propietario("Joana", "Rua Haverá sinais", 'f', "234-452-123-82", "043");
 
     Carro veiculo1 = new Carro("jws215", "preto", "fusca", primeiroPropietario);
     Carro veiculo2 = new Carro("iny294", "preto", "bmw", primeiroPropietario);
     Carro veiculo3 = new Carro("plk128", "preto", "nivus", primeiroPropietario);
 
-    Propietario segundoProprietario = new Propietario("Joana", "Rua Haverá sinais", 'f', "234-452-123-82", "043");
+    Propietario segundoProprietario = new Propietario("Luan", "Rua minha vida é um inferno", 'm', "234.256.234-82", "122");
 
     Carro veiculo4 = new Carro("jpk197", "preto", "v-40", segundoProprietario);
     Carro veiculo5 = new Carro("lkm021", "preto", "corsa", segundoProprietario);
